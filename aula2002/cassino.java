17846f58647abd623013bf51ff53ffac9cb87ee5
JOAO PEDRO CAVANHAS COLOMBO <joao.pccolombo@senacsp.edu.br>
2026-02-20 22:13:22 -0300

    vetor

---
+++ aula2002/cassino.java
@@ -0,0 +1,23 @@
+package aula2002;
+
+public class cassino {
+    public static void main(String[] args) {
+
+        // for(int i = 1; i <= 10; i++) { 
+        //     int dado = (int) (Math.random() * 126) + 1900;
+        //     System.out.println(dado);
+        // }
+        
+        // double valor = Math.pow(2, 1. / 1000);
+        // System.out.println(valor);
+        
+        int vetor[] = new int[10];
+        for (int i = 0; i < 10; i++) {
+            int dado = (int) (Math.random() * 126) + 1900;
+                vetor [i] = dado;
+        }
+        for (int i = 0; i < 10; i++) {
+            
+        }
+    }
+}
